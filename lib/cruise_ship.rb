5f217c4d4e562a7f9c7aad13ffca6850783461af
Tony Baker <this.tonybaker@gmail.com>
2019-02-19 21:19:58 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,6 +1,6 @@
  passengers = {
- suite_a: "Amanda Presley",
- suite_b: "Seymour Hoffman",
+  suite_a: "Amanda Presley",
+  suite_b: "Seymour Hoffman",
   suite_c: "Alfred Tennyson",
   suite_d: "Charlie Chaplin",
   suite_e: "Crumpet the Elf"
